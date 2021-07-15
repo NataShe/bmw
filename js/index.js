f00b6edd58e25eaa945629ee09da7549832fdc77
Natalia <natashe92@gmail.com>
2021-07-15 20:01:45 +0300

    add project

---
+++ js/index.js
@@ -0,0 +1,15 @@
+import accordeon from './accordeon.js';
+import burger from './burger.js';
+// import blockScrolled from './blockScrolled.js';
+import modal from './modal.js';
+import sendForm from './sendForm.js';
+import smoothScroll from './smoothScroll.js';
+import tabs from './tabs.js';
+
+
+accordeon();
+burger();
+modal();
+sendForm();
+smoothScroll(0.3);
+tabs();
